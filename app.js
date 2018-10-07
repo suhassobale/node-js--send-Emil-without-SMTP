de3ca94cc65ad96f485ae3a0a0b0c109e88f665d
suhassobale <suhassobale@gmail.com>
2018-10-07 16:50:51 +0530

    furst commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var sendmail = require('sendmail')();
+
+sendmail({
+    from: 'no-reply@abc.com',
+    to: < Receiver Email Address>,
+    subject: 'test sendmail',
+    html: 'Mail of test sendmail ',
+
+  }, function (err, reply) {
+    console.log(err);
+  });
